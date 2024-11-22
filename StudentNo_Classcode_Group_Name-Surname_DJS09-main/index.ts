048064a72d4587cd8509a717126a658ff3f0cfe6
Alutta <Aluttamatambo5@gmail.com>
2024-11-22 14:10:23 +0200

    Updated type annotations and added properties to reviews and you objects

--- StudentNo_Classcode_Group_Name-Surname_DJS09-main/index.ts
+++ StudentNo_Classcode_Group_Name-Surname_DJS09-main/index.ts
@@ -7,7 +7,12 @@ const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
 let isOpen: boolean
 
-const reviews = [
+const reviews :{
+    name: string;
+    stars: number;
+    loyaltyUser: boolean;
+    date: string;
+}[] =[
     {
         name: 'Sheia',
         stars: 5,
@@ -41,11 +46,13 @@ const you: {
     lastName : string;
     isReturning: boolean;
     age: number;
+    stayedAt: string[];
 } = {
     firstName: 'Bobby',
     lastName: 'Brown',
     isReturning: true,
-    age: 35
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
 console.log(you.userName)
